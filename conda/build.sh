1e61478591f4c9aa3d8940d634b02fc2e76f51de
Brandon T. Willard <brandonwillard@gmail.com>
2017-10-07 21:58:10 -0500

    fix for curl library location

--- conda/build.sh
+++ conda/build.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
-CMAKE_GENERATOR="Unix Makefiles"
-CMAKE_ARCH="-m"$ARCH
 SHORT_OS_STR=$(uname -s)
 
 if [ "${SHORT_OS_STR:0:5}" == "Linux" ]; then
-    DYNAMIC_EXT="so"
-    IS_OSX=0
+
     mkdir -p src/build
     cd src/build
 
-    # FYI: Might need the following for Ubuntu 16.04 (xenial)
-    # sudo apt-get install --reinstall libgnutls-openssl27 libcurl4-gnutls-dev
+    # XXX: Hack for clang compilation.
+    CMAKE_OPTS_EXTRA=''
+    if [ -e "$(command -v c++)" ] && c++ -v 2>&1 | grep -q clang; then
+      CMAKE_OPTS_EXTRA='-D CMAKE_TOOLCHAIN_PREFIX="llvm-" -D CMAKE_CXX_STANDARD=11' 
+    fi
 
-    # After installing, we might need to set LD_LIBRARY_PATHS when activating
-    # the conda env?
-    # See https://conda.io/docs/user-guide/tasks/manage-environments.html#macos-and-linux 
-
-    cmake ..  -G"$CMAKE_GENERATOR" \
+    cmake -G"$CMAKE_GENERATOR" $CMAKE_OPTS_EXTRA \
         -D CMAKE_BUILD_TYPE=RELEASE \
         -D CMAKE_SKIP_RPATH:bool=ON \
         -D CMAKE_INSTALL_PREFIX="$PREFIX" \
         -D CMAKE_INSTALL_SYSCONFDIR="$PREFIX/etc" \
+        -D CURL_INCLUDE_DIR:PATH="$PREFIX/include" \
+        -D CURL_LIBRARY:FILEPATH="$PREFIX/lib/libcurl.so" \
+        -D Leptonica_LIB:FILEPATH="$PREFIX/lib/liblept.so" \
+        -D Tesseract_INCLUDE_BASEAPI_DIR:PATH="$PREFIX/include" \
+        -D Tesseract_LIB:FILEPATH="$PREFIX/lib/libtesseract.so" \
         ..
 
-    #-D CMAKE_INSTALL_RPATH="$PREFIX/lib" \
-    #-D CMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE \
+        # -D log4cplus_INCLUDE_DIR:PATH="$PREFIX/include" \
+        # -D log4cplus_LIB:FILEPATH="$PREFIX/lib/liblog4cplus.so" \
+        # -D CMAKE_INSTALL_RPATH="$PREFIX/lib" \
+        # -D CMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE \
 
     make -j${CPU_COUNT}
 
@@ -34,8 +37,10 @@ if [ "${SHORT_OS_STR:0:5}" == "Linux" ]; then
     # FYI: See src/bindings/python/CMakeLists.txt; it manually installs into 
     # `${CMAKE_INSTALL_PREFIX}/lib/python2.7/dist-packages`.
     # Here's an attempted work-around for Python > 2.7:
+    # if [ "$PY_VER" -gt "2.7" ]; then 
     cd ../bindings/python
     #cd $SRC_DIR/src/bindings/python
     ${PYTHON} setup.py install || exit 1;
+    # fi
 
 fi
