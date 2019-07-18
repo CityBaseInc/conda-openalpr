a834cd4d7a0c28bf565b615a187b7790a3d88c16
Brandon T. Willard <brandonwillard@gmail.com>
2019-07-18 18:56:26 -0500

    Update build for newer conda-build capabilities
    These changes should use less of the host's libraries, or at least be more
    explicit about what it's using.

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # OpenALPR Conda Package
 
-Conda recipe for [OpenALPR](https://github.com/openalpr/openalpr) on Ubuntu (16.04-only).
+Conda recipe for [OpenALPR](https://github.com/openalpr/openalpr) on Linux.
 
+# Setup
 
+For development work, you'll need to create an Anaconda environment:
+```bash
+$ conda create -y -n test-env python==3.7 numpy matplotlib conda-build
+$ conda activate test-env
+```
+
+Everything that follows assumes that the environment `test-env` has been activated.
+
+Next, clone and [build](https://docs.conda.io/projects/conda-build/en/latest/install-conda-build.html) the `openalpr` recipe:
+```bash
+(test-env) $ git clone git@github.com:CityBaseInc/conda-openalpr.git
+(test-env) $ conda build -c conda-forge conda-openalpr
+(test-env) $ conda install --use-local -c conda-forge conda-openalpr
+```
--- conda/build.sh
+++ conda/build.sh
@@ -2,45 +2,40 @@
 
 SHORT_OS_STR=$(uname -s)
 
-if [ "${SHORT_OS_STR:0:5}" == "Linux" ]; then
+mkdir -p src/build
+cd src/build
 
-    mkdir -p src/build
-    cd src/build
+# log4cplus
+git clone https://github.com/log4cplus/log4cplus.git --branch REL_1_1_3-RC8 --single-branch --recurse-submodules
+cd log4cplus
+./configure --prefix="$PREFIX"
+make
+make install
+cd ..
 
-    # XXX: Hack for clang compilation.
-    CMAKE_OPTS_EXTRA=''
-    if [ -e "$(command -v c++)" ] && c++ -v 2>&1 | grep -q clang; then
-      CMAKE_OPTS_EXTRA='-D CMAKE_TOOLCHAIN_PREFIX="llvm-" -D CMAKE_CXX_STANDARD=11' 
-    fi
-
-    cmake -G"$CMAKE_GENERATOR" $CMAKE_OPTS_EXTRA \
-        -D CMAKE_BUILD_TYPE=RELEASE \
-        -D CMAKE_SKIP_RPATH:bool=ON \
-        -D CMAKE_INSTALL_PREFIX="$PREFIX" \
-        -D CMAKE_INSTALL_SYSCONFDIR="$PREFIX/etc" \
-        -D CURL_INCLUDE_DIR:PATH="$PREFIX/include" \
-        -D CURL_LIBRARY:FILEPATH="$PREFIX/lib/libcurl.so" \
-        -D Leptonica_LIB:FILEPATH="$PREFIX/lib/liblept.so" \
-        -D Tesseract_INCLUDE_BASEAPI_DIR:PATH="$PREFIX/include" \
-        -D Tesseract_LIB:FILEPATH="$PREFIX/lib/libtesseract.so" \
-        ..
-
-        # -D log4cplus_INCLUDE_DIR:PATH="$PREFIX/include" \
-        # -D log4cplus_LIB:FILEPATH="$PREFIX/lib/liblog4cplus.so" \
-        # -D CMAKE_INSTALL_RPATH="$PREFIX/lib" \
-        # -D CMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE \
-
-    make -j${CPU_COUNT}
-
-    make install
-    
-    # FYI: See src/bindings/python/CMakeLists.txt; it manually installs into 
-    # `${CMAKE_INSTALL_PREFIX}/lib/python2.7/dist-packages`.
-    # Here's an attempted work-around for Python > 2.7:
-    # if [ "$PY_VER" -gt "2.7" ]; then 
-    cd ../bindings/python
-    #cd $SRC_DIR/src/bindings/python
-    ${PYTHON} setup.py install || exit 1;
-    # fi
-
+# XXX: Hack for clang compilation.
+CMAKE_OPTS_EXTRA=''
+if [ -e "$(command -v c++)" ] && c++ -v 2>&1 | grep -q clang; then
+    CMAKE_OPTS_EXTRA='-D CMAKE_TOOLCHAIN_PREFIX="llvm-" -D CMAKE_CXX_STANDARD=11'
 fi
+
+cmake -G"$CMAKE_GENERATOR" $CMAKE_OPTS_EXTRA \
+    -DCMAKE_BUILD_TYPE=RELEASE \
+    -DCMAKE_SKIP_RPATH:bool=ON \
+    -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+    -DCMAKE_INSTALL_SYSCONFDIR="$PREFIX/etc" \
+    -DENABLE_CONFIG_VERIFICATION=ON \
+    -DCURL_INCLUDE_DIR:PATH="$PREFIX/include" \
+    -DCURL_LIBRARY:FILEPATH="$PREFIX/lib/libcurl.so" \
+    -DLeptonica_LIB:FILEPATH="$PREFIX/lib/liblept.so" \
+    -DTesseract_INCLUDE_BASEAPI_DIR:PATH="$PREFIX/include" \
+    -DTesseract_LIB:FILEPATH="$PREFIX/lib/libtesseract.so" \
+    -Dlog4cplus_INCLUDE_DIR:PATH="$PREFIX/include" \
+    -Dlog4cplus_LIB:FILEPATH="$PREFIX/lib/liblog4cplus.so" \
+    ..
+
+make -j${CPU_COUNT}
+make install
+
+cd ../bindings/python
+${PYTHON} setup.py install || exit 1;
