2011f6fd11d0d294ae2c87bdae3ef2a1c453f287
sergeiz2 <serge.zvenig@gmail.com>
2019-07-18 19:14:26 -0500

    Changes for using host dependencies

--- conda/build.sh
+++ conda/build.sh
@@ -10,21 +10,27 @@ if [ "${SHORT_OS_STR:0:5}" == "Linux" ]; then
     # XXX: Hack for clang compilation.
     CMAKE_OPTS_EXTRA=''
     if [ -e "$(command -v c++)" ] && c++ -v 2>&1 | grep -q clang; then
-      CMAKE_OPTS_EXTRA='-D CMAKE_TOOLCHAIN_PREFIX="llvm-" -D CMAKE_CXX_STANDARD=11' 
+      CMAKE_OPTS_EXTRA='-DCMAKE_TOOLCHAIN_PREFIX="llvm-" -DCMAKE_CXX_STANDARD=11' 
     fi
 
     cmake -G"$CMAKE_GENERATOR" $CMAKE_OPTS_EXTRA \
-        -D CMAKE_BUILD_TYPE=RELEASE \
-        -D CMAKE_SKIP_RPATH:bool=ON \
-        -D CMAKE_INSTALL_PREFIX="$PREFIX" \
-        -D CMAKE_INSTALL_SYSCONFDIR="$PREFIX/etc" \
-        -D CURL_INCLUDE_DIR:PATH="$PREFIX/include" \
-        -D CURL_LIBRARY:FILEPATH="$PREFIX/lib/libcurl.so" \
-        -D Leptonica_LIB:FILEPATH="$PREFIX/lib/liblept.so" \
-        -D Tesseract_INCLUDE_BASEAPI_DIR:PATH="$PREFIX/include" \
-        -D Tesseract_LIB:FILEPATH="$PREFIX/lib/libtesseract.so" \
+        -DCMAKE_BUILD_TYPE=RELEASE \
+        -DCMAKE_SKIP_RPATH:bool=ON \
+        -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+        -DCMAKE_INSTALL_SYSCONFDIR="$PREFIX/etc" \
+        -DCURL_INCLUDE_DIR:PATH="$PREFIX/include" \
+        -DCURL_LIBRARY:FILEPATH="$PREFIX/lib/libcurl.so" \
+        -DLeptonica_LIB:FILEPATH="$PREFIX/lib/liblept.so" \
+        -DTesseract_INCLUDE_BASEAPI_DIR:PATH="$PREFIX/include" \
+        -DTesseract_LIB:FILEPATH="$PREFIX/lib/libtesseract.so" \
+        -DTesseract_INCLUDE_CCSTRUCT_DIR="$PREFIX/include/tesseract" \
+        -DTesseract_INCLUDE_CCMAIN_DIR="$PREFIX/include/tesseract" \
+        -DTesseract_INCLUDE_CCUTIL_DIR="$PREFIX/include/tesseract" \
         ..
 
+    # Tesseract_INCLUDE_CCSTRUCT_DIR=Tesseract_INCLUDE_CCSTRUCT_DIR-NOTFOUND
+    # Tesseract_INCLUDE_CCMAIN_DIR=Tesseract_INCLUDE_CCMAIN_DIR-NOTFOUND
+    # Tesseract_INCLUDE_CCUTIL_DIR=Tesseract_INCLUDE_CCUTIL_DIR-NOTFOUND
         # -D log4cplus_INCLUDE_DIR:PATH="$PREFIX/include" \
         # -D log4cplus_LIB:FILEPATH="$PREFIX/lib/liblog4cplus.so" \
         # -D CMAKE_INSTALL_RPATH="$PREFIX/lib" \
@@ -33,7 +39,7 @@ if [ "${SHORT_OS_STR:0:5}" == "Linux" ]; then
     make -j${CPU_COUNT}
 
     make install
-    
+
     # FYI: See src/bindings/python/CMakeLists.txt; it manually installs into 
     # `${CMAKE_INSTALL_PREFIX}/lib/python2.7/dist-packages`.
     # Here's an attempted work-around for Python > 2.7:
